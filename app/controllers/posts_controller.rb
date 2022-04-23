e1bcc9c20c241b45a5d3e36185be9a65ce2b8de4
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-04-23 11:03:30 -0500

    complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,18 @@
 class PostsController < ApplicationController
-
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: "Record not found" }, status: :not_found
   end
 
   private
@@ -19,5 +20,4 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
-
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :category, inclusion: ["Non-Fiction", "Fiction"]
+  validates :content, length: { minimum: 100 }
 end
